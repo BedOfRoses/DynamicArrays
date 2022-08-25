2ff2269a7b3cda9b557f560be83f6514e4c85f96
SaraYohannes <89453142+SaraYohannes@users.noreply.github.com>
2022-08-25 12:47:29 +0200

    Index, Add number, print element fr array
    works

--- DynamicArrays/main.cpp
+++ DynamicArrays/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
 
 /*
-	//Source-help : https://www.youtube.com/watch?v=MwwbgqG6bSk
+* Sources
+*	 https://www.youtube.com/watch?v=MwwbgqG6bSk (C++ Programming Tutorials - 30 - C++ Dynamic Arrays - Eric Liang )
+*	 C++ Primer, 5th Edition, Stanley B. Lippman, Josée Lajoie, Barbara E. Moo
+*	 https://www.youtube.com/watch?v=I-hZkUa9mIs (The Cherno, Templates in C++)
+*/
 
+/* Compulsory 1
+* Add to the array
+* expand the array
+* delete from the array
+* find an element in the array
+* A search algorithms such as : linear OR Binary search
+* !!Code at least four different sort algorithms for your array 
+*	Allow user to chose which sorting array to use
+*		selection sort
+*		bubble sort
+*		merge sort
+*		quick sort
+*		heap sort
+*		(you can use other sort algorithms if you wish)
+* 
+* Write a short report 
+*	explain your approach to this problem and how you solved them
+*	how difficult or eary the task wa
+* 
+* Submission file format
+*	Individual [Firstname_Lastname].zip
+*/
+
+/*
 	datatype *TheArray
 	TheArray = new datatype[numElements];
 
@@ -10,7 +38,10 @@
 
 	*/
 
+//search function
+int binarySearch(int Element);
 
+template<typename T, int _size>
 class DynamicArray 
 {
 public:
@@ -27,26 +58,47 @@ public:
 		delete[] i_ptr_DynArray;
 	};
 
-	int AddToArray(int i_index) 
+	int AddToArray(int i_index, T element) 
 	{
-
+		return DynAmi[i_index] += element;
 	}
 
-
+	void PrintA(int i) {
+		std::cout << DynAmi[i];
+		
+	}
 
 private:
 	int *i_ptr_DynArray; //This is the pointer.
 
 	int i_index;
 
+	T DynAmi[_size];
+
 };
 
+//search function
+int binarySearch(int Element)
+{
+	
+
+	return 0;
+}
 
 
 int main() 
 { 
 
+	DynamicArray<int, 6> Bob;
 
+	Bob.AddToArray(1,0);
+	Bob.AddToArray(2,1);
+	Bob.AddToArray(3,2);
+	Bob.AddToArray(4,3);
+	Bob.AddToArray(5,4);
+	Bob.AddToArray(6,5);
+
+	Bob.PrintA(2);
 
 	return 0;
 }
